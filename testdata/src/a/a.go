5768d6a547370557893e28e653897cc417947c43
qawatake <38106890+qawatake@users.noreply.github.com>
2023-10-21 23:36:06 +0900

    add test case

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -3,10 +3,17 @@ package a
 import "fmt"
 
 func f() {
+	// limited
 	Target(nil, 2, "3") // ok
 	Target(1, "2", 3.3) // ok
 	Target(1, 1.1, "3") // want "not allowed"
 	Target(1, nil, "3") // want "not allowed"
+
+	// not limited
+	Target2(nil, 2, "3") // ok
+	Target2(1, "2", 3.3) // ok
+	Target2(1, 1.1, "3") // ok
+	Target2(1, nil, "3") // ok
 
 	// variadic
 	fmt.Println(1)           // want "not allowed"
@@ -16,5 +23,8 @@ func f() {
 
 // b must be either int or string
 func Target(a any, b any, c any) {}
+
+// b can be any type
+func Target2(a any, b any, c any) {}
 
 type MyInt int
